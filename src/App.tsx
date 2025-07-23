d66a67d4089e0a2cfcb9b5a579c7a749b210b6e3
Bohdan <bodya24102001@gmail.com>
2025-07-23 23:44:51 +0300

    prettier configured and used

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,68 @@
-import './App.css'
-import { useRef } from 'react'
-import MapSvg from './components/MapSvg/MapSvg.tsx'
+import './App.css';
+import { useRef, useState } from 'react';
+import MapSvg from './components/MapSvg/MapSvg.tsx';
 import PlayButton from './components/PlayButton/PlayButton.tsx';
 
 function App() {
   const svgRef = useRef<SVGSVGElement | null>(null);
+  const [quizText, setQuizText] = useState('Натисніть "Грати", щоб почати ⟶');
 
   // Ukrainian - english pairs of region names
-  // const regionsMap = {
-  //   Krym: "Крим",
-  //   Vinnytska: "Вінницька",
-  //   Volynska: "Волинська",
-  //   Dnipropetrovska: "Дніпропетровська",
-  //   Donetska: "Донецька",
-  //   Zhytomyrska: "Житомирська",
-  //   Zakarpatska: "Закарпатська",
-  //   Zaporizka: "Запорізька",
-  //   IvanoFrankivska: "Івано-Франківська",
-  //   Kyivska: "Київська",
-  //   Kirovohradska: "Кіровоградська",
-  //   Luhanska: "Луганська",
-  //   Lvivska: "Львівська",
-  //   Mykolaivska: "Миколаївська",
-  //   Odeska: "Одеська",
-  //   Poltavska: "Полтавська",
-  //   Rivnenska: "Рівненська",
-  //   Sumska: "Сумська",
-  //   Ternopilska: "Тернопільська",
-  //   Kharkivska: "Харківська",
-  //   Khersonska: "Херсонська",
-  //   Khmelnytska: "Хмельницька",
-  //   Cherkaska: "Черкаська",
-  //   Chernivetska: "Чернівецька",
-  //   Chernihivska: "Чернігівська",
-  //   Sevastopilska: "Севастопольська"
-  // };
+  const regionsMap = {
+    Krym: 'Крим',
+    Vinnytska: 'Вінницька',
+    Volynska: 'Волинська',
+    Dnipropetrovska: 'Дніпропетровська',
+    Donetska: 'Донецька',
+    Zhytomyrska: 'Житомирська',
+    Zakarpatska: 'Закарпатська',
+    Zaporizka: 'Запорізька',
+    IvanoFrankivska: 'Івано-Франківська',
+    Kyivska: 'Київська',
+    Kirovohradska: 'Кіровоградська',
+    Luhanska: 'Луганська',
+    Lvivska: 'Львівська',
+    Mykolaivska: 'Миколаївська',
+    Odeska: 'Одеська',
+    Poltavska: 'Полтавська',
+    Rivnenska: 'Рівненська',
+    Sumska: 'Сумська',
+    Ternopilska: 'Тернопільська',
+    Kharkivska: 'Харківська',
+    Khersonska: 'Херсонська',
+    Khmelnytska: 'Хмельницька',
+    Cherkaska: 'Черкаська',
+    Chernivetska: 'Чернівецька',
+    Chernihivska: 'Чернігівська',
+    Sevastopilska: 'Севастопольська',
+  };
+  const notGuessedRegions = Object.keys(regionsMap);
 
-  const handleDisabled = (event: React.MouseEvent<SVGPathElement>) => {
+  const handleDisabled = (event: React.MouseEvent<SVGPathElement>): void => {
     const path = event.currentTarget;
 
     if (!path.classList.contains('disabled_region')) {
       path.classList.add('disabled_region');
     }
+  };
 
-  }
+  const handlePlayGame = (): void => {
+    // Get random region to guess
+    const currentRegion =
+      notGuessedRegions[Math.floor(Math.random() * notGuessedRegions.length)];
 
-  const handleStartGame = () => {
-
-  }
+    setQuizText(`Де знаходиться ${currentRegion} область ?`);
+  };
 
   return (
     <>
       <div>
-        <PlayButton handleClick={handleStartGame} />
+        <p>{quizText}</p>
+        <PlayButton handleClick={handlePlayGame} />
         <MapSvg ref={svgRef} handleClick={handleDisabled} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
