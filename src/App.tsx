549d902167a6667980abd9517e76f3e788456918
Bohdan <bodya24102001@gmail.com>
2025-07-23 23:51:07 +0300

    regions map decomposed

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,14 @@ import './App.css';
 import { useRef, useState } from 'react';
 import MapSvg from './components/MapSvg/MapSvg.tsx';
 import PlayButton from './components/PlayButton/PlayButton.tsx';
+import { REGIONS_MAP } from './constants/regions.ts';
 
 function App() {
   const svgRef = useRef<SVGSVGElement | null>(null);
   const [quizText, setQuizText] = useState('Натисніть "Грати", щоб почати ⟶');
 
-  // Ukrainian - english pairs of region names
-  const regionsMap = {
-    Krym: 'Крим',
-    Vinnytska: 'Вінницька',
-    Volynska: 'Волинська',
-    Dnipropetrovska: 'Дніпропетровська',
-    Donetska: 'Донецька',
-    Zhytomyrska: 'Житомирська',
-    Zakarpatska: 'Закарпатська',
-    Zaporizka: 'Запорізька',
-    IvanoFrankivska: 'Івано-Франківська',
-    Kyivska: 'Київська',
-    Kirovohradska: 'Кіровоградська',
-    Luhanska: 'Луганська',
-    Lvivska: 'Львівська',
-    Mykolaivska: 'Миколаївська',
-    Odeska: 'Одеська',
-    Poltavska: 'Полтавська',
-    Rivnenska: 'Рівненська',
-    Sumska: 'Сумська',
-    Ternopilska: 'Тернопільська',
-    Kharkivska: 'Харківська',
-    Khersonska: 'Херсонська',
-    Khmelnytska: 'Хмельницька',
-    Cherkaska: 'Черкаська',
-    Chernivetska: 'Чернівецька',
-    Chernihivska: 'Чернігівська',
-    Sevastopilska: 'Севастопольська',
-  };
-  const notGuessedRegions = Object.keys(regionsMap);
+
+  const notGuessedRegions = Object.keys(REGIONS_MAP);
 
   const handleDisabled = (event: React.MouseEvent<SVGPathElement>): void => {
     const path = event.currentTarget;
